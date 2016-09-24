d3d0b63eda3e971cb0763e420f6d816243c6ae11
Sarah Harper <justalittlemajik@gmail.com>
2016-09-24 17:25:50 -0400

    users controller, model, sessions controller

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ group :development do
 end
 
 gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ end
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
