c764278943ff06c102325d2567f5f613652f3bd1
Sarah Harper <justalittlemajik@gmail.com>
2016-09-24 13:49:47 -0400

    comments controller, model, partials. seeding

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,35 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.new(comment_params)
+
+    if comment.save
+      flash[:notice] = "Comment was saved successfully."
+      redirect_to [@post]
+    else
+      flash[:alert] = "Comment failed to save."
+      redirect_to [@post]
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+
+    if comment.destroy
+      flash[:notice] = "Comment was deleted successfully."
+      redirect_to [@post]
+    else
+      flash[:alert] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post]
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+
+  has_many :comments, dependent: :destroy
+  
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
 end
