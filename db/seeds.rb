fcea59befcf95b6f2f67cbeac896955cc66d4fb5
Sarah Harper <justalittlemajik@gmail.com>
2016-09-25 14:43:28 -0400

    voting and ranking, but the ranking's not really working

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,12 +26,14 @@ end
 topics = Topic.all
 
 50.times do
-  Post.create!(
+  post = Post.create!(
   title: RandomData.random_sentence,
   body: RandomData.random_paragraph,
   topic: topics.sample,
   user: users.sample
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
@@ -70,3 +72,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
