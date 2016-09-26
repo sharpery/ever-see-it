f587a2f99b2cb73bf96f59759ad886edaee76245
Sarah Harper <justalittlemajik@gmail.com>
2016-09-26 18:43:24 -0400

    api maybe

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::UsersController < Api::V1::BaseController
+
+  before_action :authenticate_user
+  before_action :authorize_user
+
+  def show
+    user = User.find(params[:id])
+    render json: user, status: 200
+  end
+
+  def index
+    users = User.all
+    render json: users, status: 200
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user, status: 200
+    else
+      render json: { error: "User update failed", status: 400 }, status: 400
+    end
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save!
+      render json: user, status: 201
+    else
+      render json: { error: "user is invalid", satus: 400 }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :role)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,14 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create, :update]
+      resources :topics, except: [:edit, :new]
+
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
