591cef3c3cd2e2534856724dc18b549cc950a9b0
Sarah Harper <justalittlemajik@gmail.com>
2016-10-01 10:38:26 -0400

    voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -30,6 +30,6 @@ class Post < ActiveRecord::Base
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
-    update_rank(:rank, new_rank)
+    update_attribute(:rank, new_rank)
   end
 end
